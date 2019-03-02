c63852765a287506b1531ea707722d2932cfb037
Kevin Chi <kevinchi118@gmail.com>
2019-03-02 15:58:29 -0600

    removes unneeded comment

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -12,7 +12,6 @@ class Navbar extends React.Component {
     this.props.routes.map((route, index) => <li><NavLink key={index} className="navlink" to={route.path} exact activeStyle={this.current()}><span className="route-name">{route.name}</span><i className={route.icon}></i></NavLink></li>)
   )
   render () {
-    // Replace navbar with site title later
     return (
       <nav className="navbar">
         <h1>Chat'nPost<i class="far fa-comments"></i></h1>
